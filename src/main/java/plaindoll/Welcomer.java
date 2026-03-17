d55be074f960394df1628b65ca3ccfe1f0093a7b
Горшков Виктор Евгеньевич <vgorshkov@gid.ru>
2026-03-17 16:47:24 +0300

    Fix

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,25 +1,21 @@
-package plaindoll;
-
 public class Welcomer{
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
-	public String saySome(){
-		return "Ready to work, sir! Bereit zu arbeiten, Sir!";
-	}
-	// Новый метод для пункта 10 задания
+        // Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
+        // https://t.me/notopsofficial
+        public String sayWelcome() {
+                return "Welcome home, good hunter. What is it your desire?";
+        }
+        public String sayFarewell() {
+                return "Farewell, good hunter. May you find your worth in waking world.";
+        }
+        public String sayNeedGold(){
+                return "Not enough gold";
+        }
+        public String saySome(){
+                return "something in the way";
+        }
+        // Новый метод для пункта 10 задания
         public String getHunterReply() {
                 return "The hunter is always on the watch.";
         }
 }
+
