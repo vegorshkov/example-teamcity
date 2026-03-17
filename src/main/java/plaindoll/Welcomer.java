33a3862ef430a8d9b12c040e9eb8cc1cc9cedf49
Горшков Виктор Евгеньевич <vgorshkov@gid.ru>
2026-03-17 16:52:20 +0300

    Fix

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,3 +1,5 @@
+package plaindoll;
+
 public class Welcomer{
         // Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
         // https://t.me/notopsofficial
